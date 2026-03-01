780add6e7f6e9755a1fb76bc65285f50ea009cff
Aman Raj <amanraj@Amans-MacBook-Air.local>
2026-03-01 17:47:35 +0530

    Module-2.2.2 [1. Creation of ProjectController in controller package, 2. Creation of ProjectRequest, ProjectResponse, ProjectSummaryResponse in dto.project package, 3. Creation of ProjectService in service package] defining-the-rest-api-endpoints-for-the-lovable-clone-project

---
+++ src/main/java/com/aman/projects/lovable_clone/controller/ProjectController.java
@@ -0,0 +1,54 @@
+package com.aman.projects.lovable_clone.controller;
+
+import com.aman.projects.lovable_clone.dto.project.ProjectRequest;
+import com.aman.projects.lovable_clone.dto.project.ProjectResponse;
+import com.aman.projects.lovable_clone.dto.project.ProjectSummaryResponse;
+import com.aman.projects.lovable_clone.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/projects")
+@RequiredArgsConstructor
+public class ProjectController {
+
+    private final ProjectService projectService;
+
+    @GetMapping
+    //Get Projects of the user
+    public ResponseEntity<List<ProjectSummaryResponse>> getMyProjects(){
+        Long userId=1L;//TODO: update later with real Spring Security
+        return ResponseEntity.ok(projectService.getUserProjects(userId));
+    }
+
+    @GetMapping("/{id}")
+    //Get Projects of the user by id
+    public ResponseEntity<ProjectResponse> getProjectById(@PathVariable Long id){
+        Long userId=1L;
+        return ResponseEntity.ok(projectService.getProjectById(id,userId));
+    }
+
+    @PostMapping
+    //Create a project
+    public ResponseEntity<ProjectResponse> createProject(@RequestBody ProjectRequest request){
+        Long userId=1L;
+        return ResponseEntity.status(HttpStatus.CREATED).body(projectService.createProject(request, userId));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ProjectResponse> updateProject(@PathVariable Long id, @RequestBody ProjectRequest request){
+        Long userId=1L;
+        return ResponseEntity.ok(projectService.updateProject(id, request, userId));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ProjectResponse> deleteProject(@PathVariable Long id){
+        Long userId=1L;
+        projectService.softDelete(id, userId);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/aman/projects/lovable_clone/service/ProjectService.java
@@ -0,0 +1,22 @@
+package com.aman.projects.lovable_clone.service;
+
+import com.aman.projects.lovable_clone.dto.project.ProjectRequest;
+import com.aman.projects.lovable_clone.dto.project.ProjectResponse;
+import com.aman.projects.lovable_clone.dto.project.ProjectSummaryResponse;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+@Service
+public interface ProjectService {
+     List<ProjectSummaryResponse> getUserProjects(Long userId);
+
+     ProjectResponse getProjectById(Long id, Long userId);
+
+     ProjectResponse createProject(ProjectRequest request, Long userId);
+
+     ProjectResponse updateProject(Long id, ProjectRequest request, Long userId);
+
+    void softDelete(Long id, Long userId);
+}
