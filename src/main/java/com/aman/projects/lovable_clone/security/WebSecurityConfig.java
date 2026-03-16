d98c61a35c9c728efa0bf7b1773446178c25e381
Aman Raj <amanraj@Amans-MacBook-Air.local>
2026-03-16 22:03:03 +0530

    Module-4.1.5 [1. Added signup validations] exception-handling-code-clean-up-and-spring-security-added-in-lovable-clone-project

---
+++ src/main/java/com/aman/projects/lovable_clone/security/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.aman.projects.lovable_clone.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+/*CSRF protection: disabled (CSRF = Cross-Site Request Forgery/prevents malicious sites from forcing a logged-in user to perform actions)
+Sessions: disabled (Do NOT create or store HTTP sessions)
+Authentication: none
+Authorization: everyone allowed for /api/** -> (Any endpoint starting with /api/ is publicly accessible.)
+This is security bypass->Developers often write this during development to avoid authentication issues.
+CSRF protection adds a security layer against malicious requests, but it is commonly disabled in stateless REST APIs to avoid repeated authentication during development.
+*/
+
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+        httpSecurity.csrf(csrfConfig -> csrfConfig.disable())
+                .sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/**").permitAll());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
